ab5a688fb4cd0cfe031a21efad9eb5d17b368999
GaurvPatil <gaurvpatil17@gmail.com>
2023-09-06 14:30:03 +0530

    Schema and types , resolver functions and query variabales

--- index.js
+++ index.js
@@ -1,50 +1,42 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
 
-
-
-
-const typeDefs = `#graphql
-
-type Book {
-  title: String
-  author: String
-}
-
-
-type Query {
-  books: [Book]
-}
-`;
-
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
     },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
+
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
     },
-  ];
 
-  const resolvers = {
-    Query: {
-      books: () => books,
+    reviews() {
+      return db.reviews;
     },
-  };
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
 
+    authors() {
+      return db.authors;
+    },
 
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-})
+  typeDefs,
+  resolvers,
+});
 
-const {  url } = await startStandaloneServer(server , {
-    listen : {  port:4000 }
-})
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
 
-console.log("server ready at port : " ,  4000)
-
-
+console.log("server ready at port : ", 4000);
