98a4fa16a24a7aaeb27f94d973cd7f064e8c07ce
GaurvPatil <gaurvpatil17@gmail.com>
2023-09-07 23:08:59 +0530

    related data or  nested data

--- _db.js
+++ _db.js
@@ -19,7 +19,7 @@ let games = [
     {id: '4', rating: 5, content: 'lorem ipsum', author_id: '2', game_id: '4'},
     {id: '5', rating: 8, content: 'lorem ipsum', author_id: '2', game_id: '5'},
     {id: '6', rating: 7, content: 'lorem ipsum', author_id: '1', game_id: '2'},
-    {id: '7', rating: 10, content: 'lorem ipsum', author_id: '3', game_id: '1'},
+    {id: '7', rating: 10, content: 'kjbklorem ipsum', author_id: '3', game_id: '1'},
   ]
   
   export default { games, authors, reviews }
